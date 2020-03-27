e1a0ac03d4e3e1bfc040bd0ddcb1c6d7ffffbbd7
crazyturtles <rohanmatharu@gmail.com>
2020-03-27 10:59:52 -0600

    final

--- WebFormsDemo/Pages/StudentForm.aspx.cs
+++ WebFormsDemo/Pages/StudentForm.aspx.cs
@@ -7,10 +7,10 @@ using System.Web.UI.WebControls;
 
 namespace WebFormsDemo
 {
-    public partial class PersonForm : System.Web.UI.Page
+    public partial class StudentForm : System.Web.UI.Page
     {
         
-        private static List<Person> People = new List<Person>();
+        private static List<Student> People = new List<Student>();
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -18,13 +18,13 @@ namespace WebFormsDemo
         }
         protected void Add_Click(object sender, EventArgs e)
         {
-            MessageLabel.Text = "SIN = " + SIN.Text + " Name = " + Name.Text;
+            MessageLabel.Text = "StudentID = " + StudentID.Text + " Name = " + Name.Text;
             if (Page.IsValid)
             {
                 bool found = false;
                 foreach (var item in People)
                 {
-                    if (item.SIN == SIN.Text)
+                    if (item.StudentID == Convert.ToInt32(StudentID.Text))
                     {
                         found = true;
                     }
@@ -35,7 +35,7 @@ namespace WebFormsDemo
                 }
                 else
                 {
-                    Person newitem = new Person(SIN.Text, Name.Text, int.Parse(Age.Text), double.Parse(Wage.Text), Phone.Text);
+                    Student newitem = new Student(int.Parse(StudentID.Text), Name.Text, double.Parse(Credits.Text), Phone.Text);
                     People.Add(newitem);
                     PeopleGridView.DataSource = People;
                     PeopleGridView.DataBind();
@@ -44,10 +44,9 @@ namespace WebFormsDemo
         }
         protected void Clear_Click(object sender, EventArgs e)
         {
-            SIN.Text = "";
+            StudentID.Text = "";
             Name.Text = "";
-            Age.Text = "";
-            Wage.Text = "";
+            Credits.Text = "";
             Phone.Text = "";
         }
     }
---
+++ WebFormsDemo/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebFormsDemo
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+        public double Credits { get; set; }
+        public string EmergencyPhoneNumber { get; set; }
+        public Student()
+        {
+         
+        }
+        public Student(int studentid,
+                        string name,
+                        double credits,
+                        string emergencyphonenumber)
+        {
+            StudentID = studentid;
+            Name = name;
+            Credits = credits;
+            EmergencyPhoneNumber = emergencyphonenumber;
+        }
+    }
+}
